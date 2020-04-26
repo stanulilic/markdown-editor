aec811ff1317c8ad297675a62a2aee92b7e187f0
Stanley Ulili <stanleyulili@gmail.com>
2020-04-26 10:28:12 +0200

    (feat): add a texteditor component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import TextEditor from './TextEditor';
 
-const name = "stanley ulili"
 const App = () => {
     return (
-    <div>
-    <h1>Hello World</h1>
-    <p>{name}</p>
+    <div className="editor-container">
+        <div class="split">
+         <TextEditor />
+        </div>
     </div>
     );
 };
---
+++ src/TextEditor.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TextEditor = () => {
+    return (
+        <div className="editor">
+            <textarea class="markdown-input">
+            </textarea>
+        </div>
+    )
+}
+
+export default TextEditor;
