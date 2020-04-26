d4c9ef0a3e4a9b204d880aa743d27c55df62c875
Stanley Ulili <stanleyulili@gmail.com>
2020-04-26 10:42:55 +0200

    (feat): add initial state for the texteditor component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TextEditor from './TextEditor';
 
+
 const App = () => {
+    const initialMarkdownText = `
+    # This is a heading
+     Type what you got in your mind
+
+     The following is a link: [link text](https://www.reddit.com)
+
+     list items:
+
+     - houses
+     - cars
+     - animals
+
+     This is a paragraph
+
+     ## This is second heading
+    `
+    const [text, setText] = useState(initialMarkdownText);
     return (
     <div className="editor-container">
         <div class="split">
-         <TextEditor />
+         <TextEditor text={text} />
         </div>
     </div>
     );
--- src/TextEditor.js
+++ src/TextEditor.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const TextEditor = () => {
+const TextEditor = (props) => {
     return (
         <div className="editor">
             <textarea class="markdown-input">
+                {props.text}
             </textarea>
         </div>
     )
