3e7d7c542ac3227bc8bfc08160ac2bb9e69ae2c1
Stanley Ulili <stanleyulili@gmail.com>
2020-05-16 09:44:05 +0200

    (feat): format text links when format link url button is clicked

--- src/Nav.js
+++ src/Nav.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Modal from './Modal.js';
 import { ReactComponent as BoldIcon} from './icons/bold.svg';
 import { ReactComponent as LinkIcon} from './icons/chain.svg';
@@ -23,7 +23,7 @@ import { ReactComponent as CloseIcon } from './icons/close.svg';
 const IconElement = (props) => {
     return (
             <div>
-                <button onClick={props.handleClick} className="navbar__button" title={props.title} arial-label={props.label}>
+                <button onClick={props.handleClick} className="navbar__button" title={props.title} arial-label={props.label} id={props.id}>
                 {props.children}
                 </button>
             </div>
@@ -33,13 +33,6 @@ const IconElement = (props) => {
 const EditingIcons = (props) => {
 
 
-    const getTextArea = () => {
-        const editorWrapperNode = props.textAreaRef.current;
-        const  textAreaElement = editorWrapperNode.querySelector('textarea');
-        return textAreaElement;
-
-    }
-
     const changeSelectedText = (textArea, startPos, endPos, enclosingTextStart, enclosingTextEnd) => {
             const selectedText = textArea.value.slice(startPos, endPos);
             const output = `${enclosingTextStart}${selectedText}${enclosingTextEnd}`
@@ -48,7 +41,7 @@ const EditingIcons = (props) => {
 
     const formatMarkdownCode = ()  => {
         let currentValue = null;
-        const textArea = getTextArea();
+        const textArea = props.getTextArea();
         let text = "`enter code here`";
         const startPos = textArea.selectionStart;
         const endPos = textArea.selectionEnd;
@@ -78,7 +71,7 @@ const EditingIcons = (props) => {
     }
     const formatText = (defaultText, enclosingTextStart, enclosingTextEnd) => {
         let currentValue = null;
-        const textArea = getTextArea();
+        const textArea = props.getTextArea();
         let text = defaultText;
         const startPos = textArea.selectionStart;
         const endPos = textArea.selectionEnd;
@@ -112,7 +105,7 @@ const EditingIcons = (props) => {
             <IconElement  handleClick={() => {formatText("1. list item one\n2. list item two", "1. ", "")}} title="Ordered List" label="Ordered List"><OlListIcon /></IconElement>
             <IconElement  handleClick={() => {formatText("> Blockquote", "> ", "")}} title="Block Quote" label="Block Quote"><BlockQuoteIcon /></IconElement>
             <IconElement  handleClick={() => { formatMarkdownCode()}}  title="Code" label="Code"><CodeIcon /></IconElement>
-            <IconElement  handleClick={() => {props.toggleModal()}} title="Link" label="Link"><LinkIcon /></IconElement>
+            <IconElement  handleClick={() => {props.toggleModal()}} title="Link" label="Link" id="format-link"><LinkIcon /></IconElement>
             <IconElement  title="Image" label="Image"><ImageIcon /></IconElement>
         </div>
     )
@@ -130,36 +123,95 @@ const NavbarMainTools = () => {
 
 const Nav = (props) => {
     const [ showModal, setModal] = useState(false);
+    const [ markdownImageLink, setMarkdownImagelink] = useState(false);
+    const [ description, setDescription] = useState("");
 
-    const toggleModal = () => setModal(!showModal);
+
+    useEffect(() => {
+        // check if markdown link button is clicked
+        if(!markdownImageLink) {
+            setDescription("Please provide a URL for your link.");
+        }
+
+    }, [markdownImageLink]);
+
+    const toggleModal = () => {
+        setModal(!showModal)
+    };
+    const getTextArea = () => {
+        const editorWrapperNode = props.textAreaRef.current;
+        const  textAreaElement = editorWrapperNode.querySelector('textarea');
+        return textAreaElement;
+
+    }
+
+
+    const formatMarkdownLink = (type, url) => {
+        let currentValue = null;
+        const textArea = getTextArea();
+        let text = null;
+        if(type === "anchor") {
+            text = "enter link description here";
+        }
+        const startPos = textArea.selectionStart;
+        const endPos = textArea.selectionEnd;
+        const textBeforeCursorPosition = textArea.value.substring(0, startPos)
+        const textAfterCursorPosition = textArea.value.substring(endPos, textArea.value.length)
+        const lines = textArea.value.substring(0, startPos).split("\n");
+         
+        if(startPos !== endPos) {
+            text = textArea.value.slice(startPos, endPos);
+        }
+        if(lines[lines.length - 1 ]) {
+            currentValue  = `${textBeforeCursorPosition} [${text}](${url}) ${textAfterCursorPosition}`; 
+        }else{
+            currentValue  = `${textBeforeCursorPosition}\n[${text}](${url})\n${textAfterCursorPosition}`; 
+        }
+
+        textArea.value = currentValue;
+        props.updateMarkdownState(currentValue);
+        toggleModal();
+    }
+
     return (
         <header>
             <nav className="navbar">
                 <EditingIcons textAreaRef={props.textAreaRef} 
                 toggleModal={toggleModal} 
+                getTextArea={getTextArea}
                 updateMarkdownState={props.updateMarkdownState} />
                 <NavbarMainTools />
                 { showModal ? (
                     <Modal>
                     <div className="modal__wrap">
-                        <div className="modal__content">
+                        <form onSubmit={event => {
+                            event.preventDefault();
+                            // check if markdown link button is clicked
+                            if(!markdownImageLink) {
+                                formatMarkdownLink("anchor", event.target.elements.url.value);
+                            }
+                        }} 
+                        className="modal__content">
                          <div className="modal__header">
-                            <button  onClick={() => {toggleModal()}} className="modal__close-btn" id="close-modal" title="close modal" arial-label="Close Modal">
+                            <button  onClick={() => {toggleModal()}}
+                             className="modal__close-btn" id="close-modal" 
+                             title="close modal" arial-label="Close Modal">
                           <CloseIcon />
                         </button>
                         </div>
                         <div className="modal__body">
-                          <p>Please provide a <b>URL</b> for your link.</p>
+                          <p>{description}</p>
                             <div className="modal__form">
                             <label htmlFor="url" className="modal__label">URL</label>
                             <input type="url" name="url" className="modal__input" />
                             </div>
                         </div>
                         <div className="modal__footer modal_buttons">
-                            <button onClick={() => {toggleModal()}} className="modal-footer__btn modal__cancel-btn">Cancel</button>
-                            <button className="modal-footer__btn modal__okay-btn">Ok</button>
+                            <button onClick={() => {toggleModal()}} 
+                            className="modal-footer__btn modal__cancel-btn">Cancel</button>
+                            <button className="modal-footer__btn modal__okay-btn" type="submit">Ok</button>
                         </div>
-                        </div>
+                        </form>
                     </div>
                     </Modal>
                 ): null}
