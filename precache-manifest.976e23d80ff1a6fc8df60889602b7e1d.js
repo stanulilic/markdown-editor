40f7801b25f7f1525bd8f181af2fb72cc4939e8a
Stanley Ulili <stanleyulili@gmail.com>
2020-06-04 21:23:29 +0200

    Updates

---
+++ precache-manifest.976e23d80ff1a6fc8df60889602b7e1d.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d1bcadf05eb19c91519b6296ec66aa",
+    "url": "/markdown-editor/index.html"
+  },
+  {
+    "revision": "ee30ac36c005e6cabfef",
+    "url": "/markdown-editor/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "53fde1bce96deb37cd1d",
+    "url": "/markdown-editor/static/css/main.1293f75a.chunk.css"
+  },
+  {
+    "revision": "ee30ac36c005e6cabfef",
+    "url": "/markdown-editor/static/js/2.4bcb12ec.chunk.js"
+  },
+  {
+    "revision": "6de07e5aec012398ec95763a9e7531f9",
+    "url": "/markdown-editor/static/js/2.4bcb12ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53fde1bce96deb37cd1d",
+    "url": "/markdown-editor/static/js/main.c42a9dbd.chunk.js"
+  },
+  {
+    "revision": "9896b7f8f183694c7b8c",
+    "url": "/markdown-editor/static/js/runtime-main.7e32ca7b.js"
+  },
+  {
+    "revision": "e9e4a6b394cae9945a01299642b87907",
+    "url": "/markdown-editor/static/media/bold.e9e4a6b3.svg"
+  },
+  {
+    "revision": "a128cf3267a00d106ab7d62ab16f0e91",
+    "url": "/markdown-editor/static/media/chain.a128cf32.svg"
+  },
+  {
+    "revision": "765ad40b3af472bb1b9c544e24e62af5",
+    "url": "/markdown-editor/static/media/chevron-up.765ad40b.svg"
+  },
+  {
+    "revision": "017fbe80c84afa40c590df6f20df8421",
+    "url": "/markdown-editor/static/media/close.017fbe80.svg"
+  },
+  {
+    "revision": "9b40332e6901a8585ce013c2b9e61067",
+    "url": "/markdown-editor/static/media/code.9b40332e.svg"
+  },
+  {
+    "revision": "7e0506007585aa8d9b5dc1a8d7ca6305",
+    "url": "/markdown-editor/static/media/columns.7e050600.svg"
+  },
+  {
+    "revision": "84848dbcd604607ee99a942b6711220e",
+    "url": "/markdown-editor/static/media/download.84848dbc.svg"
+  },
+  {
+    "revision": "d815af9d29ca1b2a48f4224d914da60b",
+    "url": "/markdown-editor/static/media/embed.d815af9d.svg"
+  },
+  {
+    "revision": "0524b0ad7e66d101ffa19e0b52f88c06",
+    "url": "/markdown-editor/static/media/eye.0524b0ad.svg"
+  },
+  {
+    "revision": "74a5c2bdaec1de745b72076e270c0587",
+    "url": "/markdown-editor/static/media/file-image-o.74a5c2bd.svg"
+  },
+  {
+    "revision": "ef63b5e5b7018ec5e3dc768e4e619309",
+    "url": "/markdown-editor/static/media/header.ef63b5e5.svg"
+  },
+  {
+    "revision": "2bba5de003f31a00bf4cee1d00451636",
+    "url": "/markdown-editor/static/media/italic.2bba5de0.svg"
+  },
+  {
+    "revision": "27e90ed9323c4c8f74b687de71959eb9",
+    "url": "/markdown-editor/static/media/list-ol.27e90ed9.svg"
+  },
+  {
+    "revision": "a5d9e8664b1c06a734f54e78ffbecd14",
+    "url": "/markdown-editor/static/media/list-ul.a5d9e866.svg"
+  },
+  {
+    "revision": "8cf082c00dac81e42294c2ed0167ea90",
+    "url": "/markdown-editor/static/media/markdown.8cf082c0.svg"
+  },
+  {
+    "revision": "0be50337d209bfef7e6ce071a37ee0a0",
+    "url": "/markdown-editor/static/media/pdf.0be50337.svg"
+  },
+  {
+    "revision": "9a0351259a270dd50b4a9f7fa266ac19",
+    "url": "/markdown-editor/static/media/pencil.9a035125.svg"
+  },
+  {
+    "revision": "63c6337e4734738b67c5e730b424ec9e",
+    "url": "/markdown-editor/static/media/quote-right.63c6337e.svg"
+  },
+  {
+    "revision": "6d33ce58e2d0b008b87682c7d0d6327b",
+    "url": "/markdown-editor/static/media/redo2.6d33ce58.svg"
+  },
+  {
+    "revision": "ca13f5871b1b6c0ebf00dbdf1205ad1f",
+    "url": "/markdown-editor/static/media/strikethrough.ca13f587.svg"
+  },
+  {
+    "revision": "c5c830600610b6f57b6385e63fe702a9",
+    "url": "/markdown-editor/static/media/undo2.c5c83060.svg"
+  },
+  {
+    "revision": "e36a1f7685fb026f19900682b82f9293",
+    "url": "/markdown-editor/static/media/upload2.e36a1f76.svg"
+  }
+]);
